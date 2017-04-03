e41285bc2924491387f4e72088e8babdbe6e4b52
alexsandro giovanni dos santos filho <tudomaistv@gmail.com>
2017-04-03 14:08:01 -0300

    primerio commit

---
+++ Source/Prova/ProvaGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProvaGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROVA_API AProvaGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
