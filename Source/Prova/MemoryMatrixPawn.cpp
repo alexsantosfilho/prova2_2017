daacecc3a29b5f40fc061f2f367a1646237cc71d
alexsandro giovanni dos santos filho <tudomaistv@gmail.com>
2017-04-03 14:35:48 -0300

    add Pawn, mapa e camera

---
+++ Source/Prova/MemoryMatrixPawn.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Prova.h"
+#include "MemoryMatrixPawn.h"
+
+
+// Sets default values
+AMemoryMatrixPawn::AMemoryMatrixPawn()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->ProjectionMode = ECameraProjectionMode::Orthographic;
+	Camera->OrthoWidth = 2048.0f;
+	Camera->SetupAttachment(RootComponent);
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void AMemoryMatrixPawn::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMemoryMatrixPawn::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AMemoryMatrixPawn::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
---
+++ Source/Prova/MemoryMatrixPawn.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Pawn.h"
+#include "MemoryMatrixPawn.generated.h"
+
+UCLASS()
+class PROVA_API AMemoryMatrixPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AMemoryMatrixPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	
+private:
+
+	UPROPERTY(EditAnywhere)
+		UCameraComponent* Camera;
+	
+};
